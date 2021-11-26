c471f28cbdf50bee86556c5e3c34cdc24b077820
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-11-26 23:18:53 +0100

    Fix basepath config for prod

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,9 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-	plugins: [tailwindcss, autoprefixer, !dev && cssnano()]
-};
+    plugins: [tailwindcss, autoprefixer, !dev && cssnano()],
+}
--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,17 @@ import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 import { resolve } from 'path'
 
+const dev = process.env.NODE_ENV === 'development'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: preprocess({ postcss: true }),
     kit: {
         target: '#svelte',
         adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/christmas-tree',
+        },
         vite: {
             resolve: {
                 alias: {
