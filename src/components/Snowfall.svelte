c8e39d0561dbea9b94dd7b53c1e4f0cf5d028221
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-11-26 21:51:41 +0100

    Add snowfall and split into components

---
+++ src/components/Snowfall.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" context="module">
+    const snowflakes = [...Array(250).keys()]
+</script>
+
+<div class="snowfall">
+    {#each snowflakes as i (i)}
+        <div class="snowflake" />
+    {/each}
+</div>
